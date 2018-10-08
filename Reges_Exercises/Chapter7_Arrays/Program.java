5ef48f777214865e6c329cee91a0fc595ac2a60c
Frederik <Frederiklundbeck@live.dk>
2018-10-08 19:00:51 +0200

    Updated

--- Reges_Exercises/Chapter7_Arrays/Program.java
+++ Reges_Exercises/Chapter7_Arrays/Program.java
@@ -4,13 +4,31 @@ public class Program
 {
     public static void main(String[] args) 
     {
-        int[] numbers1 = new int[] { 74, 85, 102, 99, 101, 85, 56};
+        //Exercise 1 test
+        int[] numbers1 = new int[] { 74, 85, 102, 99, 101, 85, 56 };
 
         System.out.println(lastIndexOf(numbers1, 85));
 
+        //Exercise 2 test
         int[] numbers2 = new int[] { 36, 12 ,25 ,19 ,46 ,31 ,22 };
 
         System.out.println(range(numbers2));
+
+        //Exercise 3 test
+        int[] numbers3 = new int[] { 14, 1, 22, 17, 36, 7, -37, 5 };
+
+        System.out.println(countInRange(numbers3, 4, 17));
+
+        //Exercise 4 test
+        double[] numbers4 = new double[] { 16.1, 12.3, 22.2, 14.4 };
+        double[] numbers5 = new double[] { 1.5, 4.3, 7.0, 19.5, 25.1, 46.2 };
+
+        System.out.println(isSorted(numbers4));
+        System.out.println(isSorted(numbers5));
+
+        //Exercise 5 test
+        
+
     }
 
     //Exercise 1
@@ -39,6 +57,8 @@ public class Program
         return max - min + 1;
     }
 
+    //Method that returns the smallest value of a given int array 
+
     public static int getSmallestValue(int[] numbers)
     {
         int shortestIndex = 0;
@@ -53,6 +73,8 @@ public class Program
         return numbers[shortestIndex];
     }
 
+    //Method that returns the biggest value of a given int array
+
     public static int getBiggestValue(int[] numbers)
     {
         int biggestIndex = 0;
@@ -65,6 +87,37 @@ public class Program
             }
         }
         return numbers[biggestIndex];
+    }
+
+
+    //Exercise 3
+
+    public static int countInRange(int[] numbers, int min, int max)
+    {
+        int count = 0;
+
+        for (int i = 0; i < numbers.length; i++) 
+        {
+            if (numbers[i] >=  min && numbers[i] <= max) 
+            {
+                count++;    
+            }
+        }  
+        return count;         
+    }
+
+    //Exercise 4
+
+    public static boolean isSorted(double[] numbers)
+    {
+        for (int i = 1; i < numbers.length; i++) 
+        {
+            if (numbers[i] < numbers[i-1]) 
+            {
+                return false;
+            }    
+        }
+        return true;
     }
 
 }
