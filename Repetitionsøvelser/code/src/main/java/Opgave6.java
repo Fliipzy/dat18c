e67cb8799f37a18d640d1924463a3056b2dab7c5
Frederik <Frederiklundbeck@live.dk>
2018-12-02 13:37:18 +0100

    added opgave 6

---
+++ Repetitionsøvelser/code/src/main/java/Opgave6.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Opgave6 
+{    
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            Scanner scanner = new Scanner(new File("tekst/Opgave6Tekst.txt"));
+            collapseSpaces(scanner);
+        } 
+        catch (FileNotFoundException e) 
+        {
+            System.out.println("File was not found!");
+        }
+    }
+
+    public static void collapseSpaces(Scanner scanner)
+    {
+        ArrayList<String> words = new ArrayList<String>();
+
+        scanner.useDelimiter(" ");
+
+        while (scanner.hasNext()) 
+        {
+            String word = scanner.next();
+
+            if (!word.equals("")) 
+            {
+                words.add(word);
+            }
+            
+        }
+
+        for (String word : words) 
+        {
+            System.out.print(word + " ");    
+        }       
+    }
+}
---
+++ Repetitionsøvelser/code/src/main/java/Opgave7.java
@@ -0,0 +1,7 @@
+public class Opgave7 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
